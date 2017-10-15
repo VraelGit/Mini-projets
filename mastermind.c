a328d52310424485eb31278c2ad95ea104ff1938
VraelGit <32813249+VraelGit@users.noreply.github.com>
2017-10-15 22:47:21 +0200

    Add files via upload

---
+++ mastermind.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+    int essais,nombre1,nombre2,nombre3,nombre4;
+    char zf;
+
+printf("************************************\n");
+printf("****\t\t\t\t****\n");
+printf("****\t     MASTERMIND\t\t****\n");
+printf("****\t\t\t\t****\n");
+printf("************************************\n");
+printf("Vous avez 10 essais pour trouver une combinaison de 4 chiffres.\n");
+printf("Les chiffres sont compris entre 1 et 6\n");
+printf("ATTEENTION : un chiffre ne peut etre utilise que une seule fois\n");
+printf("Exemple de combinaison : 2 6 5 4\n");
+printf("COMBINAISON SECRETE : \n");
+printf("Entrer une combinaison : ");
+scanf("%d",&nombre1);
+return 0;
+}
